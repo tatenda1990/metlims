13100677cdabb234280119715fb2ecdeea87ceb8
Collins Saguru <collins@cm-solutions.co.za>
2025-05-17 04:38:01 +0200

    Initial project setup with backend and frontend

---
+++ backend/metlims_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for metlims_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metlims_backend.settings')
+
+application = get_asgi_application()
